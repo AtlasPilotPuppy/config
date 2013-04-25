075ef2c1c4e10bc4e1cb5b2a8829a740610beacd
Anant <anant.asty@gmail.com>
2013-04-25 14:12:16 -0600

    Commit for Thu Apr 25 14:12:16 MDT 2013

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]
 then
+    dpkg --get-selections > ~/Package.list
     sudo cp /etc/apt/sources.list ~/sources.list
     sudo apt-key exportall > ~/Repo.keys
     rsync -az --progress --exclude 'Downloads' --exclude 'Music' --exclude 'Videos' --exclude 'Public' --exclude 'Pictures' --exclude 'Mail' --exclude '*chromium*' --exclude '*thunderbird*' --exclude '*mozilla*'  --exclude '*cache*' -r /home/anant/ /media/anant/Seagate\ Backup\ Plus\ Drive/Backup/profile
